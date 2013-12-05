543102a1149e545f01092a7b5f7794586cef5c1b
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2013-12-05 07:04:29 -0300

    Melhoria na legibilidade do código

--- src/main/java/adventure/service/JSEntityService.java
+++ src/main/java/adventure/service/JSEntityService.java
@@ -1,27 +1,48 @@
 package adventure.service;
 
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+
 import java.lang.reflect.Field;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
-import adventure.entity.Pessoa;
+import org.jboss.resteasy.annotations.cache.Cache;
+
 import br.gov.frameworkdemoiselle.util.Reflections;
 
-@Path("/js/entity/{entity}.js")
+@Path("/js/model/{entity}.js")
 @Produces("text/javascript")
+@Cache(maxAge = 10)
 public class JSEntityService {
 
 	@GET
-	public String getEntityMetadata(@PathParam("entity") String entity) throws ClassNotFoundException {
-		Class<?> type = Class.forName(Pessoa.class.getPackage().getName() + "." + entity);
+	public Response getEntityMetadata(@PathParam("entity") String entity) {
+		try {
+			return Response.ok(parse(entity)).build();
+		} catch (ClassNotFoundException cause) {
+			return Response.status(NOT_FOUND).build();
+		}
+	}
+
+	private static String parse(String entity) throws ClassNotFoundException {
+		Class<?> type = Class.forName("adventure.entity." + entity);
+
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("var ");
+		buffer.append(type.getSimpleName());
+		buffer.append("=function(){");
 
 		for (Field field : Reflections.getNonStaticFields(type)) {
-			System.out.println("..... " + field.getName());
+			buffer.append("this.");
+			buffer.append(field.getName());
+			buffer.append(";");
 		}
 
-		return null;
+		buffer.append("};");
+		return buffer.toString();
 	}
 }
--- src/main/webapp/js/model/pessoa.js
+++
@@ -1,8 +0,0 @@
-var Pessoa = function() {
-	this.id;
-	this.nome;
-	this.email;
-	this.senha;
-	this.nascimento;
-	this.sexo;
-};
