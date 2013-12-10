6c3add1ebe93033d734e84e36fa71bac25ff1ac9
lusabo <lucianosantosborges@gmail.com>
2013-12-10 15:36:16 -0300

    Buscando todos os eventos. Navegando pela paginação

--- src/main/java/adventure/scrap/AdventureScrap.java
+++ src/main/java/adventure/scrap/AdventureScrap.java
@@ -16,22 +16,37 @@ public class AdventureScrap {
 
 	@Startup
 	public static List<Evento> getEventFromAdventureMag() {
+		List<String> urls = new ArrayList<String>();
+		Document doc, doc2, docevento;
+		Elements pagination, elements;
+		
 		try {
-			Document doc = Jsoup.connect("http://www.adventuremag.com.br/corridadeaventura/calendario/").get();
-			Elements elements = doc.select("#colunaesquerda table table tr td:last-child a");
-			
-			for (Element element : elements) {
-				
-				Document docevento = Jsoup.connect(element.attr("abs:href")).get();
-				String nome = docevento.select(".titulo_nomeevento").text();
-				String data = docevento.select(".titulo_nomelocal span").text();
-				String local = docevento.select(".titulo_nomelocal:not(span)").text().split("-")[1].trim();
-				String link = docevento.select("#data a").get(0).attr("abs:href");
-				
-				Evento e = new Evento(nome, data, local, link);
-				eventos.add(e);
+			urls.add("http://www.adventuremag.com.br/corridadeaventura/calendario/index.php?page=1");
+			doc = Jsoup.connect(urls.get(0)).timeout(0).get();
+			pagination = doc.select(".pagination").get(0).getElementsByTag("a");
+
+			for (int i = 0; i < pagination.size() - 1; i++) {
+				urls.add(pagination.get(i).attr("abs:href"));
 			}
-			
+
+			for (String url : urls){
+				doc2 = Jsoup.connect(url).get();
+				elements = doc2.select("#colunaesquerda table table tr td:last-child a");
+				for (Element element : elements) {
+					docevento = Jsoup.connect(element.attr("abs:href")).get();
+					String nome  = docevento.select(".titulo_nomeevento").text();
+					String data  = docevento.select(".titulo_nomelocal span").text();
+					String local = docevento.select(".titulo_nomelocal:not(span)").text().split("-")[1].trim();
+					String link  = docevento.select("#data a").get(0).attr("abs:href");
+//					System.out.println("-------------------------------------------------------------");
+//					System.out.println(nome);
+//					System.out.println(data);
+//					System.out.println(local);
+//					System.out.println(link);
+					Evento e = new Evento(nome, data, local, link);
+					eventos.add(e);
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
