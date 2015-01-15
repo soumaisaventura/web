d7caf4df9d568836214d5ec0d5dc4aa766b51f5f
lusabo <lucianosantosborges@gmail.com>
2015-01-15 12:00:08 -0200

    Ajustes na tela de registro.

--- src/main/webapp/js/controller/signup.js
+++ src/main/webapp/js/controller/signup.js
@@ -1,27 +1,43 @@
 $(function() {
 	
-	App.loadDateCombos($("#birthday-day"), $("#birthday-month"), $("#birthday-year"))
+	App.loadDateCombos($("#birthday"), $("#birthday-month"), $("#birthday-year"))
 	
 	$("#name").focus();
 
 	$("form").submit(function(event) {
 		event.preventDefault();
 		$("[id$='-message']").hide();
+		
+		var birthday = "";
+		var year = $("#birthday-year").val();
+		var month = $("#birthday-month").val();
+		var day = $("#birthday").val();
+		
+		if (!isNaN(year) && !isNaN(month) && !isNaN(day)){
+			console.log('data válida');
+			birthday = new Date(year, month, day);
+		}
 
 		var data = {
 			'name' : $("#name").val(),
 			'password' : $("#password").val(),
 			'email' : $("#email").val(),
-			'birthday' : $("#birthday").val(),
+			'birthday' : birthday,
 			'gender' : $("#gender").val()
 		};
-
+		console.log('--- 1 ---');
+		console.log(data);
 		SignUpProxy.signup(data).done(signupOk);
 	});
 });
 
 // SignUp process
-
 function signupOk(data, status, request) {
-	alert("Cadastro efetuado. Siga as instruções no seu e-mail para ativar a sua conta.");
+	console.log('--- 2 ---');
+	console.log(data);
+	console.log(status);
+	console.log(request);
+	$("#global-message").addClass('alert-success')
+						.text("Cadastro efetuado. Siga as instruções no seu e-mail para ativar a sua conta.")
+						.show();
 }
