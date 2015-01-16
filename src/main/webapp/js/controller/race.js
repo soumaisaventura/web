9f8d62f6b9a3c1662ff3ff4ae001e484af54ac8e
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2015-01-16 18:51:24 -0300

    Obtendo dinamicamente o raceId.

--- src/main/webapp/js/controller/race.js
+++ src/main/webapp/js/controller/race.js
@@ -1,37 +1,44 @@
 $(function() {
 	// RaceProxy.findNext().done(findNextOk);
-	RaceProxy.getBanner(1).done(getBannerOk);
-	RaceProxy.load(1).done(loadOk);
-	$("#bt-registration").click(function(){
-		location.href = "3/registration";
+
+	RaceProxy.getBanner($("#race").val()).done(getBannerOk);
+	RaceProxy.load($("#race").val()).done(loadOk);
+
+	$("#bt-registration").click(function() {
+		location.href = $("#race").val() + "/registration";
 	});
 });
 
-function getBannerOk(data){
-	$("#race-banner").attr("src","data:image/png;base64,"+data);
+function getBannerOk(data) {
+	$("#race-banner").attr("src", "data:image/png;base64," + data);
 }
 
-function loadOk(data){
+function loadOk(data) {
 	$("#race-name").prepend(data.name);
 	$("#race-description").prepend(data.description);
 	$("#race-date").prepend(moment(data.date, "YYYY-MM-DD").locale("pt-br").format('L'));
 	$("#race-city").prepend(data.city);
-	
-	if (data.registration.open) {
-		$("#race-registration-open").addClass('label label-success').text("Abertas");
-	} else {
-		$("#race-registration-open").addClass('label label-danger').text("Encerradas");
-	}
-	
-	$.each(data.registration.periods, function(index, value){
-		$("#race-registration-periods").append("<h4>" + moment(value.beginning, "YYYY-MM-DD").locale("pt-br").format('DD/MM') + " á " + moment(value.end, "YYYY-MM-DD").locale("pt-br").format('DD/MM') + " - R$ " + value.price + " <span style='font-size:0.8em; color:green'>(+anuidade)</span></h4>")
+
+	// if (data.registration.open) {
+	// $("#race-registration-open").addClass('label
+	// label-success').text("Abertas");
+	// } else {
+	// $("#race-registration-open").addClass('label
+	// label-danger').text("Encerradas");
+	// }
+
+	$.each(data.registration.periods, function(index, value) {
+		$("#race-registration-periods").append(
+				"<h4>" + moment(value.beginning, "YYYY-MM-DD").locale("pt-br").format('DD/MM') + " á "
+						+ moment(value.end, "YYYY-MM-DD").locale("pt-br").format('DD/MM') + " - R$ " + value.price
+						+ " <span style='font-size:0.8em; color:green'>(+anuidade)</span></h4>")
 	});
-	
+
 	var category = "";
-	$.each(data.courses, function(index, value){
+	$.each(data.courses, function(index, value) {
 		category += "<h4>" + value.length + " km</h4>";
 		category += "<ul>";
-		$.each(value.categories, function(index, value){
+		$.each(value.categories, function(index, value) {
 			category += "<li>" + value.name + " <span title='" + value.description + "'><span></li>";
 		});
 		category += "</ul>";
