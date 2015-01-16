fc5b161fea7e7675b6e7a457026e433fe7f54ecd
lusabo <lucianosantosborges@gmail.com>
2015-01-16 16:16:14 -0200

    Finalização layout tela detalhes da corrida.

--- src/main/webapp/js/controller/race.js
+++ src/main/webapp/js/controller/race.js
@@ -2,6 +2,9 @@ $(function() {
 	// RaceProxy.findNext().done(findNextOk);
 	RaceProxy.getBanner(1).done(getBannerOk);
 	RaceProxy.load(1).done(loadOk);
+	$("#bt-registration").click(function(){
+		location.href = "3/registration";
+	});
 });
 
 function getBannerOk(data){
@@ -12,6 +15,7 @@ function loadOk(data){
 	$("#race-name").prepend(data.name);
 	$("#race-description").prepend(data.description);
 	$("#race-date").prepend(moment(data.date, "YYYY-MM-DD").locale("pt-br").format('L'));
+	$("#race-city").prepend(data.city);
 	
 	if (data.registration.open) {
 		$("#race-registration-open").addClass('label label-success').text("Abertas");
@@ -19,8 +23,18 @@ function loadOk(data){
 		$("#race-registration-open").addClass('label label-danger').text("Encerradas");
 	}
 	
-	$.each(data.registration.periods, function(index ,value){
-		$("#race-registration-periods").append("<h4>" + moment(value.beginning, "YYYY-MM-DD").locale("pt-br").format('DD/MM') + " á " + moment(value.end, "YYYY-MM-DD").locale("pt-br").format('DD/MM') + " - R$ " + value.price + " (+anuidade)</h4>")
-	})
+	$.each(data.registration.periods, function(index, value){
+		$("#race-registration-periods").append("<h4>" + moment(value.beginning, "YYYY-MM-DD").locale("pt-br").format('DD/MM') + " á " + moment(value.end, "YYYY-MM-DD").locale("pt-br").format('DD/MM') + " - R$ " + value.price + " <span style='font-size:0.8em; color:green'>(+anuidade)</span></h4>")
+	});
 	
+	var category = "";
+	$.each(data.courses, function(index, value){
+		category += "<h4>" + value.length + " km</h4>";
+		category += "<ul>";
+		$.each(value.categories, function(index, value){
+			category += "<li>" + value.name + " <span title='" + value.description + "' class='glyphicon glyphicon-info-sign'><span></li>";
+		});
+		category += "</ul>";
+	});
+	$("#race-categories").after(category);
 }
