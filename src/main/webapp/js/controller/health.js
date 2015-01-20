0a060c3caa527def201999f6bd23af12721b429f
lusabo <lucianosantosborges@gmail.com>
2015-01-20 17:34:14 -0200

    Criação da tela de dados médicos.

--- src/main/webapp/js/controller/health.js
+++ src/main/webapp/js/controller/health.js
@@ -1,3 +1,55 @@
 $(function() {
 	
+	/**
+	 * Cadastro dos dados médicos
+	 */
+	$('#bt-save').on('click', function(e) {
+		var data = {
+			'bloodType' : $("#bloodType").val(),
+			'allergy' : $("#allergy").val(),
+			'healthCareName' : $("#healthCareName").val(),
+			'healthCareNumber' : $("#healthCareNumber").val(),
+			'emergencyContactName' : $("#emergencyContactName").val(),
+			'emergencyContactPhoneNumber' : $("#emergencyContactPhoneNumber").val(),
+		};
+		HealthProxy.update(data).done(updateOk).fail(updateFail);
+	});
+	
+	/**
+	 * Carrega os dados médicos
+	 */
+	HealthProxy.load().done(loadOk);
+
 });
+
+/* ---------------- Funções de Callback ---------------- */
+
+/**
+ * Funçao que carrega os dados médicos do usuário.
+ */
+function loadOk(data) {
+	console.log('loadOk');
+	$("#bloodType").val(data.bloodType);
+	$("#allergy").val(data.allergy);
+	$("#healthCareName").val(data.healthCareName);
+	$("#healthCareNumber").val(data.healthCareNumber);
+	$("#emergencyContactName").val(data.emergencyContactName);
+	$("#emergencyContactPhoneNumber").val(data.emergencyContactPhoneNumber);
+}
+
+/**
+ * 
+ */
+function updateOk(data) {
+	console.log('updateOk');
+	$("[id$='-message'").hide();
+	$("#global-message").text("Dados atualizados com sucesso.").addClass("alert-success").show();
+}
+
+/**
+ * Tratamento de erro dos dados básicos.
+ */
+function updateFail(request) {
+	console.log('updateFail');
+	$("#global-message").text("").removeClass("alert-success").hide();
+}
--- src/main/webapp/js/controller/race-registration.js
+++ src/main/webapp/js/controller/race-registration.js
@@ -31,16 +31,6 @@ $(function() {
 	 */
 	RaceProxy.findCourses($("#race").val()).done(loadComboCategoriesOk);
 
-	/**
-	 * Carrega os dados pessoais
-	 */
-	ProfileProxy.load().done(loadStep1Ok);
-
-	/**
-	 * Carrega os dados médicos
-	 */
-	HealthProxy.load().done(loadStep2Ok);
-
 	$("#name").focus();
 
 	/**
@@ -100,20 +90,7 @@ $(function() {
 
 	$('ul.setup-panel li.active a').trigger('click');
 
-	/**
-	 * Cadastro dos dados médicos
-	 */
-	$('#activate-step-3').on('click', function(e) {
-		var data = {
-			'bloodType' : $("#bloodType").val(),
-			'allergy' : $("#allergy").val(),
-			'healthCareName' : $("#healthCareName").val(),
-			'healthCareNumber' : $("#healthCareNumber").val(),
-			'emergencyContactName' : $("#emergencyContactName").val(),
-			'emergencyContactPhoneNumber' : $("#emergencyContactPhoneNumber").val(),
-		};
-		HealthProxy.update(data).done(updateStep2Ok).fail(updateStep2Fail);
-	});
+	
 
 	/**
 	 * Cadastro dos dados da equipe
@@ -135,19 +112,6 @@ $(function() {
 /* ---------------- Funções de Callback ---------------- */
 
 /**
- * Funçao que carrega os dados médicos do usuário.
- */
-function loadStep2Ok(data) {
-	console.log(data);
-	$("#bloodType").val(data.bloodType);
-	$("#allergy").val(data.allergy);
-	$("#healthCareName").val(data.healthCareName);
-	$("#healthCareNumber").val(data.healthCareNumber);
-	$("#emergencyContactName").val(data.emergencyContactName);
-	$("#emergencyContactPhoneNumber").val(data.emergencyContactPhoneNumber);
-}
-
-/**
  * Monta a caixa de seleção das categorias disponíveis da corrida. TODO Pensar
  * numa estrutura para pegar a quantidade de membros da corrida.
  */
@@ -164,18 +128,6 @@ function loadComboCategoriesOk(data) {
 	});
 }
 
-
-
-/**
- * 
- */
-function updateStep2Ok(data) {
-	console.log('updateStep2Ok');
-	$('ul.setup-panel li:eq(1)').addClass('disabled');
-	$('ul.setup-panel li:eq(2)').removeClass('disabled');
-	$('ul.setup-panel li a[href="#step-3"]').trigger('click');
-}
-
 /**
  * 
  */
@@ -184,40 +136,6 @@ function updateStep3Ok(data) {
 	$('ul.setup-panel li:eq(2)').addClass('disabled');
 	$('ul.setup-panel li:eq(3)').removeClass('disabled');
 	$('ul.setup-panel li a[href="#step-4"]').trigger('click');
-}
-
-
-
-/**
- * Tratamento de erro dos dados médicos.
- */
-function updateStep2Fail(request) {
-	console.log('updateFail');
-	switch (request.status) {
-		case 422:
-			$($("#form-step-2 input").get().reverse()).each(function() {
-				var id = $(this).attr('id');
-				var message = null;
-
-				$.each(request.responseJSON, function(index, value) {
-					if (id == value.property) {
-						message = value.message;
-						return;
-					}
-				});
-
-				if (message) {
-					$("#" + id + "-message").html(message).show();
-					$(this).focus();
-				} else {
-					$("#" + id + "-message").hide();
-				}
-			});
-			break;
-
-		default:
-			break;
-	}
 }
 
 /**
