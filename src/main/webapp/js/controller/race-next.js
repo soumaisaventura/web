9c1d7ef19fe9fdb093f0168ac6db172a99d4566b
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2015-01-22 06:35:28 -0300

    De /profile e /health para /user/profile e /user/health além de pequenos ajustes em outras telas

--- src/main/webapp/js/controller/race-next.js
+++ src/main/webapp/js/controller/race-next.js
@@ -7,10 +7,10 @@ function findNextOk(data) {
 		var day = moment(value.date, "YYYY-MM-DD");
 
 		var td1 = '<td class="text-capitalize text-center"><h1>' + day.date() + '</h1>' + day.locale("pt-br").format("MMMM") + '</td>';
-		var td2 = '<td><h3><a href="race/' + value.id + '">' + value.name + '</a>';
+		var td2 = '<td><h3><a href="' + App.getContextPath() + '/race/' + value.id + '">' + value.name + '</a>';
 
 		if (value.registration.open) {
-			td2 += ' <span class="label label-success">Insrições abertas</span>';
+			td2 += ' <span class="label label-danger">Insrições abertas</span>';
 		}
 
 		td2 += '</h3><small>' + (value.city ? value.city : '') + '</small></td>';
