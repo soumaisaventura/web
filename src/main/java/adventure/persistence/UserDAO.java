06d5b57f12317fea86bd6e72d046b32ec58de8ff
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2015-01-22 15:07:40 -0300

    Validação de membro com pendências cadastrais

--- src/main/java/adventure/persistence/UserDAO.java
+++ src/main/java/adventure/persistence/UserDAO.java
@@ -129,10 +129,19 @@ public class UserDAO extends JPACrud<User, Integer> {
 	public User loadBasics(Integer id) {
 		StringBuffer jpql = new StringBuffer();
 		jpql.append(" select ");
-		jpql.append(" 	new User(u.id, u.email, p.name, p.gender) ");
+		jpql.append(" 	 new User( ");
+		jpql.append(" 	     u.id, ");
+		jpql.append(" 	     u.email, ");
+		jpql.append(" 	     p.name, ");
+		jpql.append(" 	     p.gender, ");
+		jpql.append(" 	     p.pendencies, ");
+		jpql.append(" 	     h.pendencies ");
+		jpql.append(" 	     ) ");
 		jpql.append("   from Profile p");
-		jpql.append("   join p.user u ");
-		jpql.append("  where u.deleted is null ");
+		jpql.append("   join p.user u, ");
+		jpql.append("        Health h ");
+		jpql.append("  where u = h.user ");
+		jpql.append("    and u.deleted is null ");
 		jpql.append("    and u.id = :id");
 
 		TypedQuery<User> query = getEntityManager().createQuery(jpql.toString(), User.class);
@@ -150,10 +159,19 @@ public class UserDAO extends JPACrud<User, Integer> {
 	public User loadBasics(String email) {
 		StringBuffer jpql = new StringBuffer();
 		jpql.append(" select ");
-		jpql.append(" 	new User(u.id, u.email, p.name, p.gender) ");
+		jpql.append(" 	 new User( ");
+		jpql.append(" 	     u.id, ");
+		jpql.append(" 	     u.email, ");
+		jpql.append(" 	     p.name, ");
+		jpql.append(" 	     p.gender, ");
+		jpql.append(" 	     p.pendencies, ");
+		jpql.append(" 	     h.pendencies ");
+		jpql.append(" 	     ) ");
 		jpql.append("   from Profile p");
-		jpql.append("   join p.user u ");
-		jpql.append("  where u.deleted is null ");
+		jpql.append("   join p.user u, ");
+		jpql.append("        Health h ");
+		jpql.append("  where u = h.user ");
+		jpql.append("    and u.deleted is null ");
 		jpql.append("    and u.email = :email");
 
 		TypedQuery<User> query = getEntityManager().createQuery(jpql.toString(), User.class);
