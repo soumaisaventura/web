5fc602d0d426d7fb907c2e90d55792f1ec038329
lusabo <lucianosantosborges@gmail.com>
2015-01-27 17:43:35 -0200

    Lista de gerenciamento de inscrições estática.

--- src/main/webapp/js/controller/race-registration-list.js
+++ src/main/webapp/js/controller/race-registration-list.js
@@ -2,8 +2,12 @@ $(function() {
 	moment.locale("pt-br");
 	var $race = $("#race").val();
 
+	$('.footable').footable();
+	
 	RaceProxy.loadSummary($race).done(loadOk);
-	RaceRegistrationProxy.find($race).done(findOk);
+	//RaceRegistrationProxy.find($race).done(findOk);
+	
+	
 });
 
 function loadOk($data) {
@@ -13,16 +17,19 @@ function loadOk($data) {
 }
 
 function findOk($data, $status, $request) {
+	console.log($data);
 	switch ($request.status) {
 		case 204:
 			var message = "Nenhuma equipe se inscreveu ainda."
-			$('#resultTable > tbody:last').append('<tr><td>' + message + '</td></tr>');
+			$('#resultTable > tbody:last').append('<tr><td colspan="6">' + message + '</td></tr>');
 
 			break;
 
 		case 200:
 			$.each($data, function(index, value) {
-				$('#resultTable > tbody:last').append('<tr><td>' + value.id + " – " + value.teamName + " – " + value.status + '</td></tr>');
+				$('#resultTable > tbody:last')
+					.append('<tr>')
+					.append('<td>' + value.id + " – " + value.teamName + " – " + value.status + '</td></tr>');
 			});
 			break;
 	}
