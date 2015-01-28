fcd23c321d924bd995dc5f34fffc939d79f037c3
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2015-01-28 13:47:32 -0300

    atualizando o ddl

--- src/db/4-ddl-registration-approver.sql
+++ src/db/4-ddl-registration-approver.sql
@@ -12,3 +12,26 @@ ALTER TABLE public.registration ADD CONSTRAINT fk_registration_approver FOREIGN 
 REFERENCES public.user_account (id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 
+
+ALTER TABLE public.registration DROP CONSTRAINT IF EXISTS fk_registration_user CASCADE;
+
+DROP INDEX IF EXISTS public.idx_registration_user CASCADE;
+
+
+CREATE INDEX idx_registration_submitter ON public.registration
+	USING btree
+	(
+	  submitter_id
+	)	WITH (FILLFACTOR = 90);
+
+
+CREATE INDEX idx_registration_approver ON public.registration
+	USING btree
+	(
+	  approver_id ASC NULLS LAST
+	);
+
+ALTER TABLE public.registration ADD CONSTRAINT fk_registration_submitter FOREIGN KEY (submitter_id)
+REFERENCES public.user_account (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
