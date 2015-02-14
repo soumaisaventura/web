158be290ce9d5eb1d68fed41281d4373b20b48fa
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2015-02-14 10:57:09 -0300

    Definindo máscara para os campos tel

--- src/main/webapp/js/lib/app.js
+++ src/main/webapp/js/lib/app.js
@@ -159,3 +159,15 @@ $.ajaxSetup({
 		}
 	}
 });
+
+$('document').ready(function() {
+	var maskBehavior = function(val) {
+		return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+	}, options = {
+		onKeyPress : function(val, e, field, options) {
+			field.mask(maskBehavior.apply({}, arguments), options);
+		}
+	};
+
+	$('input[type="tel"]').mask(maskBehavior, options);
+});
