c0cf576ff7ed52232dc7a69842fb6d610e8532d1
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2015-02-27 20:01:17 -0300

    Atualização do Ogg

---
+++ src/main/java/adventure/helper/RaceHelper.java
@@ -0,0 +1,41 @@
+package adventure.helper;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+import adventure.entity.Race;
+import adventure.persistence.RaceDAO;
+
+@Named
+@RequestScoped
+public class RaceHelper {
+
+	private boolean initialized = false;
+
+	private String title;
+
+	private String description;
+
+	public void initialize(HttpServletRequest request) {
+		if (!initialized) {
+			initialized = true;
+
+			String param = request.getParameter("race");
+			Race race = RaceDAO.getInstance().load(Integer.parseInt(param));
+
+			this.title = race.getName();
+			this.description = race.getDescription();
+		}
+	}
+
+	public String getTitle(HttpServletRequest request) {
+		initialize(request);
+		return title;
+	}
+
+	public String getDescription(HttpServletRequest request) {
+		initialize(request);
+		return description;
+	}
+}
