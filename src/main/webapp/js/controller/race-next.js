2969b15ca5df956b8676d50298b47b70d364e0e2
Luciano Borges <lucianosantosborges@gmail.com>
2015-03-05 20:24:28 -0300

    Adicionado o ribbon nos banners.

--- src/main/webapp/js/controller/race-next.js
+++ src/main/webapp/js/controller/race-next.js
@@ -1,20 +1,30 @@
 $(function() {
+
 	$("#race-next-menu-item").addClass("active");
 
 	RaceProxy.findNext().done(findNextOk);
 
+//	$('#open-races').on('mouseover', '.panel', function() {
+//		$(this).unwrap("<div style='background-color: black; z-index: 2;'></div>");
+//	});
+	
+//	$('#open-races').on('mouseout', '.panel', function() {
+//		$(this).wrapInner("<div 'background-color: black; z-index: 2;'></div>");
+//	});
+	
 	$('#open-races').on('click', '.panel', function() {
+		
 		location.href = App.getContextPath() + "/prova/" + $(this).data("race");
 	});
 
-	// $('#open-races').on('mouseover', '.panel', function() {
-	// $(this).css('cursor', 'pointer');
-	// $("#banner-" + $(this).data("race")).fadeTo(0, 0.75);
-	// });
-	//
-	// $('#open-races').on('mouseout', '.panel', function() {
-	// $("#banner-" + $(this).data("race")).fadeTo(0, 1);
-	// });
+	 $('#open-races').on('mouseover', '.panel', function() {
+		 $(this).css('cursor', 'pointer');
+//		 $("#banner-" + $(this).data("race")).fadeTo(0, 0.75);
+	 });
+	
+//	 $('#open-races').on('mouseout', '.panel', function() {
+//	 $("#banner-" + $(this).data("race")).fadeTo(0, 1);
+//	 });
 
 });
 
@@ -44,10 +54,6 @@ function findNextOk(data) {
 					data,
 					function(index, value) {
 
-						// RaceProxy.getBanner(value.id).done(function(data) {
-						// carregarBanner(value.id, data);
-						// });
-
 						var day = moment(value.date, "YYYY-MM-DD");
 
 						var race = "";
@@ -55,30 +61,26 @@ function findNextOk(data) {
 						race += "<div class='panel panel-default' data-race='" + value.id + "'>";
 						race += "<div class='panel-heading' style='padding:0'>";
 
-						// race += "<a href='" + App.getBaseUrl() + "/prova/" +
-						// value.id + "' class='darken'>";
+						if(value.registration.open)
+						race += "<div class='box'><div class='corner'><span>Inscrições abertas</span></div>";
+
 						race += "<img class='responsive' id='banner-" + value.id + "' data-src='" + App.getBaseUrl() + "/api/race/" + value.id
 								+ "/banner/{breakpoint-name}'/>";
-						// race += "</a>";
-
+						race += "</div>";
 						race += "</div>";
 						race += "<div class='panel-body' style='padding-top: 5px'>";
-
 						race += "<div class='row'>";
 						race += "<div class='col-md-12 text-right'>";
-
 						race += "<h5 class='pull-left' style='margin-top: 10px; margin-bottom: 5px;'><span class='glyphicon glyphicon-calendar' style='font-size: 0.8em'></span> "
 								+ day.date() + ' ' + day.locale("pt-br").format("MMMM") + "</h5>";
-
 						race += "<h5 class='pull-right' style='margin-top: 10px; margin-bottom: 5px;'><span class='glyphicon glyphicon-map-marker' style='font-size: 0.8em'></span> "
 								+ (value.city ? value.city : 'Local não definido') + "</h5>";
 						race += "</div>";
 						race += "</div>";
 						race += "</div>";
 						race += "</div>";
-
+						race += "</div>";
 						$('#open-races').append(race);
-
 					});
 }
 
