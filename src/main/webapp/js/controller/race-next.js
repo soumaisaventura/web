5fb463d5982839b8d16d3d20f43aff82c3a5dd17
Luciano Borges <lucianosantosborges@gmail.com>
2015-03-05 00:25:58 -0300

    Ajustes no calendário de corridas.

--- src/main/webapp/js/controller/race-next.js
+++ src/main/webapp/js/controller/race-next.js
@@ -3,56 +3,57 @@ $(function() {
 
 	RaceProxy.findNext().done(findNextOk);
 
-	$('#open-races').on('click', '.registration', function() {
-		location.href = App.getContextPath() + '/prova/' + $(this).data("race") + '/inscricao';
+	$('#open-races').on('click', '.panel', function() {
+		location.href = App.getContextPath() + "/prova/" + $(this).data("race");
 	});
+
+	$('#open-races').on('mouseover', '.panel', function() {
+		$(this).css('cursor', 'pointer');
+		$("#banner-" + $(this).data("race")).fadeTo("fast", 0.75);
+	});
+
+	$('#open-races').on('mouseout', '.panel', function() {
+		$("#banner-" + $(this).data("race")).fadeTo("fast", 1);
+	});
+
 });
 
 function findNextOk(data) {
-	$.each(data, function(index, value) {
+	$
+			.each(
+					data,
+					function(index, value) {
 
-		RaceProxy.getBanner(value.id).done(function(data) {
-			carregarBanner(value.id, data);
-		});
+						RaceProxy.getBanner(value.id).done(function(data) {
+							carregarBanner(value.id, data);
+						});
 
-		console.log(value);
+						var day = moment(value.date, "YYYY-MM-DD");
 
-		var day = moment(value.date, "YYYY-MM-DD");
+						var race = "";
+						race += "<div class='race col-md-4'>";
+						race += "<div class='panel panel-default' data-race='" + value.id + "'>";
+						race += "<div class='panel-heading' style='padding:0'>";
+						race += "<img id='banner-" + value.id + "' src='' style='width: 100%;' />";
+						race += "</div>";
+						race += "<div class='panel-body' style='padding-top: 5px'>";
 
-		var race = "";
-		race += "<div class='col-md-4'>";
-		race += "<div class='panel panel-default'>";
-		race += "<div class='panel-heading' style='padding:0'>";
-		race += "<img id='banner-" + value.id + "' src='' style='width: 100%;' />";
-		race += "</div>";
-		race += "<div class='panel-body' style='padding-top: 5px'>";
-		race += "<h5 style='margin-top: 10px; margin-bottom: 5px;'><span class='glyphicon glyphicon-calendar' style='font-size: 0.8em'></span> "
-				+ day.date() + ' ' + day.locale("pt-br").format("MMMM") + "</h5>";
-		race += "<h5 style='margin-top: 10px; margin-bottom: 5px;'><span class='glyphicon glyphicon-map-marker' style='font-size: 0.8em'></span> "
-				+ (value.city ? value.city : 'Local não definido') + "</h5>";
-		race += "</div>";
-		race += "<div class='panel-footer' style='background-color: white;'>";
-		race += "<div class='row'>";
-		race += "<div class='col-md-6'>";
-		race += "<a href='" + App.getContextPath() + "/prova/" + value.id + "' class='registration btn btn-block btn-default'>";
-		race += "<span class='glyphicon glyphicon-eye-open' aria-hidden='true' style='font-size: 0.8em;'></span>";
-		race += " Ver detalhes";
-		race += "</a>";
-		race += "</div>";
-		race += "<div class='col-md-6'>";
-		if (value.registration.open) {
-			race += "<button type='button' class='registration btn btn-block btn-success' data-race='" + value.id + "'>";
-			race += "<span class='glyphicon glyphicon-pencil' aria-hidden='true' style='font-size: 0.8em;'></span>";
-			race += " Inscrições";
-			race += "</button>";
-		}
-		race += "</div>";
-		race += "</div>";
-		race += "</div>";
+						race += "<div class='row'>";
+						race += "<div class='col-md-12 text-right'>";
 
-		$('#open-races').append(race);
+						race += "<h5 class='pull-left' style='margin-top: 10px; margin-bottom: 5px;'><span class='glyphicon glyphicon-calendar' style='font-size: 0.8em'></span> "
+								+ day.date() + ' ' + day.locale("pt-br").format("MMMM") + "</h5>";
 
-	});
+						race += "<h5 class='pull-right' style='margin-top: 10px; margin-bottom: 5px;'><span class='glyphicon glyphicon-map-marker' style='font-size: 0.8em'></span> "
+								+ (value.city ? value.city : 'Local não definido') + "</h5>";
+						race += "</div>";
+						race += "</div>";
+						race += "</div>";
+						race += "</div>";
+
+						$('#open-races').append(race);
+
+					});
 }
 
 function carregarBanner(id, data) {
@@ -60,7 +61,7 @@ function carregarBanner(id, data) {
 	if (data) {
 		banner = "data:image/png;base64," + data;
 	} else {
-		banner = "http://placehold.it/350x150";
+		banner = "http://placehold.it/750x350";
 	}
 	$("#banner-" + id).attr("src", banner);
 }
