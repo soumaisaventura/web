1f4b490fa513280c111407c82344846f34fcff16
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2015-04-07 16:12:58 -0300

    Mudança para funcionar no mobile

--- src/main/webapp/js/proxy/race.js
+++ src/main/webapp/js/proxy/race.js
@@ -23,14 +23,15 @@ var RaceProxy = {
 		var xhr = new XMLHttpRequest();
 		xhr.open("GET", this.url + "/" + raceId + "/form", true);
 		xhr.responseType = 'blob';
-		xhr.onload = function(e) {
-			if (this.status === 200) {
+		// xhr.onload = function() {
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
 				var blob = new Blob([ this.response ], {
 					type : this.response.type
 				});
 
-				var url = URL.createObjectURL(blob);
-				window.open(url, "_blank");
+				var url = (window.URL || window.webkitURL).createObjectURL(blob);
+				location.href = url;
 			}
 		};
 		App.setHeader(xhr)
