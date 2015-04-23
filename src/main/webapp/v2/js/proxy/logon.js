5f104aeaf13244aac19f93e6ba6c5482c5e8a683
lusabo <lucianosantosborges@gmail.com>
2015-04-23 17:18:30 -0300

    Adicionado os js para a página de login.

---
+++ src/main/webapp/v2/js/proxy/logon.js
@@ -0,0 +1,41 @@
+var LogonProxy = {
+
+	url : App.getContextPath() + "/api/logon",
+
+	login : function($data) {
+		return $.ajax({
+			type : "POST",
+			url : this.url,
+			data : JSON.stringify($data),
+			contentType : "application/json",
+			error : function() {}
+		});
+	},
+
+	getOAuthAppIds : function() {
+		return $.ajax({
+			type : "GET",
+			url : this.url + "/oauth"
+		});
+	},
+
+	facebookLogin : function($data) {
+		return $.ajax({
+			type : "POST",
+			url : this.url + "/oauth/facebook",
+			data : JSON.stringify($data),
+			contentType : "application/json",
+			processData : false,
+		});
+	},
+
+	googleLogin : function($data) {
+		return $.ajax({
+			type : "POST",
+			url : this.url + "/oauth/google",
+			data : JSON.stringify($data),
+			contentType : "application/json",
+			processData : false,
+		});
+	},
+};
