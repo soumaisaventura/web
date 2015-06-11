49e4b2cb7157ecf9e449792a3d17c92c07b2a298
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2015-06-11 09:45:21 -0300

    Ajuste nas fichas de inscrição

--- src/db/misc.sql
+++ src/db/misc.sql
@@ -3,17 +3,35 @@ SELECT *
  WHERE ua.email = 'monelopes17@gmail.com';
 
 SELECT *
-  FROM profile p
- WHERE lower (p.name) LIKE '%neto%';
+  FROM user_account ua
+ WHERE id = 789;
 
 SELECT *
   FROM profile p
- WHERE p.id = 639;
+ WHERE lower (p.name) LIKE '%claudionor%';
+
+SELECT *
+  FROM profile p
+ WHERE p.id = 789;
 
 SELECT *
   FROM health h
- WHERE h.id = 639;
+ WHERE h.id = 581;
 
 SELECT *
-  FROM team_formation tf
- WHERE tf.user_id IN (639);
+  FROM team_formation tf, profile p
+ WHERE tf.user_id = p.id AND tf.registration_id = 166;
+
+UPDATE team_formation
+   SET user_id = 789
+ WHERE registration_id = 166 AND user_id = 38;
+
+
+DELETE FROM profile p
+      WHERE p.id = 581;
+
+DELETE FROM health h
+      WHERE h.id = 581;
+
+DELETE FROM user_account ua
+      WHERE id = 581;
