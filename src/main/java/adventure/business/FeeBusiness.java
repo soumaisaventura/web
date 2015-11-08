0ad3e280d74752006fb2e90bdb5d78d02e6b000e
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2015-11-08 13:53:59 -0300

    Finalização da implementação do cálculo de taxas: Implementação de GET /api/event/{id}
    Task-Url:

--- src/main/java/adventure/business/FeeBusiness.java
+++ src/main/java/adventure/business/FeeBusiness.java
@@ -1,7 +1,9 @@
 package adventure.business;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import adventure.entity.Fee;
 import br.gov.frameworkdemoiselle.util.Beans;
@@ -14,36 +16,43 @@ public class FeeBusiness {
 
 	public BigDecimal applyForEvent(BigDecimal price, List<Fee> raceFees, List<Fee> championshipFees) {
 		BigDecimal result = price;
-		result = result.add(compute(price, championshipFees));
-		result = result.add(compute(price, raceFees));
+		Set<Fee> computedFees = new HashSet<Fee>();
+
+		result = result.add(compute(price, championshipFees, computedFees));
+		result = result.add(compute(price, raceFees, computedFees));
 		return result;
 	}
 
-	private BigDecimal compute(BigDecimal price, List<Fee> fees) {
+	private BigDecimal compute(BigDecimal price, List<Fee> fees, Set<Fee> computed) {
 		BigDecimal result = BigDecimal.ZERO;
 
 		if (fees != null) {
 			for (Fee fee : fees) {
-				result = result.add(compute(price, fee));
+				result = result.add(compute(price, fee, computed));
 			}
 		}
 
 		return result;
 	}
 
-	private BigDecimal compute(BigDecimal price, Fee fee) {
+	private BigDecimal compute(BigDecimal price, Fee fee, Set<Fee> computed) {
 		BigDecimal result;
 
-		if (!fee.getOptional()) {
+		boolean bypass = false;
+		bypass |= fee.getOptional();
+		bypass |= (fee.getOnce() != null && fee.getOnce());
+
+		if (bypass) {
+			result = BigDecimal.ZERO;
+		} else {
 			if (fee.getPercentage()) {
 				result = price.multiply(fee.getValue()).divide(BigDecimal.valueOf(100));
 			} else {
 				result = fee.getValue();
 			}
-		} else {
-			result = BigDecimal.ZERO;
 		}
 
+		computed.add(fee);
 		return result;
 	}
 }
