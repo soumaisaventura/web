c3d3ab6f7bb7642038fe01e162e339a4a1ae11c2
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2015-11-12 16:29:16 -0300

    Mais experimentos com o maps

--- src/main/webapp/js/controller/map.js
+++ src/main/webapp/js/controller/map.js
@@ -1,4 +1,90 @@
-$(function() {});
+var map;
+
+$(function() {
+	$("#buscar").click(function() {
+		var marker;
+		var coord;
+		var coords = [];
+
+		// Cangaço
+		coord = new google.maps.LatLng(-11.9778555, -39.102922);
+		coords.push(coord);
+		marker = new google.maps.Marker({
+			position : coord,
+			map : map
+		});
+
+		// Desafio dos Sertões
+		coord = new google.maps.LatLng(-9.4093282, -40.5070731);
+		coords.push(coord);
+		marker = new google.maps.Marker({
+			position : coord,
+			map : map
+		});
+
+		// Casco de Peba
+		coord = new google.maps.LatLng(-9.3932462, -40.4839331);
+		coords.push(coord);
+		marker = new google.maps.Marker({
+			position : coord,
+			map : map
+		});
+
+		// Noite do Perrengue
+		coord = new google.maps.LatLng(-12.406472, -37.9067442);
+		coords.push(coord);
+		marker = new google.maps.Marker({
+			position : coord,
+			map : map
+		});
+
+		// Corrida do CT Gantuá
+		coord = new google.maps.LatLng(-13.0073514, -41.369959);
+		coords.push(coord);
+		marker = new google.maps.Marker({
+			position : coord,
+			map : map
+		});
+
+		// Peleja
+		coord = new google.maps.LatLng(-12.5471833, -38.7102718);
+		coords.push(coord);
+		marker = new google.maps.Marker({
+			position : coord,
+			map : map
+		});
+
+		// Laskpé
+		coord = new google.maps.LatLng(-9.4886588, -40.6168342);
+		coords.push(coord);
+		marker = new google.maps.Marker({
+			position : coord,
+			map : map
+		});
+
+		// Native
+		coord = new google.maps.LatLng(-10.7540411, -36.9578873);
+		coords.push(coord);
+		marker = new google.maps.Marker({
+			position : coord,
+			map : map
+		});
+
+		// Mandacaru
+		coord = new google.maps.LatLng(-12.1236112, -39.1098222);
+		coords.push(coord);
+		marker = new google.maps.Marker({
+			position : coord,
+			map : map
+		});
+
+		var bounds = new google.maps.LatLngBounds();
+		for (var i = 0, size = coords.length; i < size; i++) {
+			bounds.extend(coords[i]);
+		}
+		map.fitBounds(bounds);
+	});
+});
 
 function initMap() {
 	if (navigator.geolocation) {
@@ -20,92 +106,8 @@ function centerMap(position) {
 			lat : position.coords.latitude,
 			lng : position.coords.longitude
 		},
-		zoom : 6,
+		zoom : 6
 	};
 
-	var map = new google.maps.Map($("#map")[0], options);
-	// markerClusterer = new MarkerClusterer(map);
-
-	var marker;
-	var coord;
-	var coords = [];
-
-	// Cangaço
-	coord = new google.maps.LatLng(-11.9778555, -39.102922);
-	coords.push(coord);
-	marker = new google.maps.Marker({
-		position : coord,
-		map : map
-	});
-
-	// Desafio dos Sertões
-	coord = new google.maps.LatLng(-9.4093282, -40.5070731);
-	coords.push(coord);
-	marker = new google.maps.Marker({
-		position : coord,
-		map : map
-	});
-
-	// Casco de Peba
-	coord = new google.maps.LatLng(-9.3932462, -40.4839331);
-	coords.push(coord);
-	marker = new google.maps.Marker({
-		position : coord,
-		map : map
-	});
-
-	// Noite do Perrengue
-	coord = new google.maps.LatLng(-12.406472, -37.9067442);
-	coords.push(coord);
-	marker = new google.maps.Marker({
-		position : coord,
-		map : map
-	});
-
-	// Corrida do CT Gantuá
-	coord = new google.maps.LatLng(-13.0073514, -41.369959);
-	coords.push(coord);
-	marker = new google.maps.Marker({
-		position : coord,
-		map : map
-	});
-
-	// Peleja
-	coord = new google.maps.LatLng(-12.5471833, -38.7102718);
-	coords.push(coord);
-	marker = new google.maps.Marker({
-		position : coord,
-		map : map
-	});
-
-	// Laskpé
-	coord = new google.maps.LatLng(-9.4886588, -40.6168342);
-	coords.push(coord);
-	marker = new google.maps.Marker({
-		position : coord,
-		map : map
-	});
-
-	// Native
-	coord = new google.maps.LatLng(-10.7540411, -36.9578873);
-	coords.push(coord);
-	marker = new google.maps.Marker({
-		position : coord,
-		map : map
-	});
-
-	// Mandacaru
-	coord = new google.maps.LatLng(-12.1236112, -39.1098222);
-	coords.push(coord);
-	marker = new google.maps.Marker({
-		position : coord,
-		map : map
-	});
-
-	var bounds = new google.maps.LatLngBounds();
-	for (var i = 0, size = coords.length; i < size; i++) {
-		bounds.extend(coords[i]);
-	}
-	// Fit these bounds to the map
-	map.fitBounds(bounds);
+	map = new google.maps.Map($("#map")[0], options);
 }
