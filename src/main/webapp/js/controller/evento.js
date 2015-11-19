ddf75b35570b12c9110d00490b0651f3558bd449
lusabo <lucianosantosborges@gmail.com>
2015-11-19 10:16:54 -0300

    Layout da coluna de evento e suas corridas.

--- src/main/webapp/js/controller/evento.js
+++ src/main/webapp/js/controller/evento.js
@@ -64,12 +64,20 @@ function loadEventOk(data) {
 		race.idx = i + 1;
 		race.day = moment(race.period.beginning, "YYYY-MM-DD").locale("pt-br").format('DD');
 		race.month = moment(race.period.beginning, "YYYY-MM-DD").locale("pt-br").format('MMM');
+		
+		race.moreThanOneDay = !(race.period.beginning === race.period.end);
+		race.period.beginning = moment(race.period.beginning, "YYYY-MM-DD").locale("pt-br").format('L');
+		race.period.end = moment(race.period.end, "YYYY-MM-DD").locale("pt-br").format('L');
+		
+		race.current_period.end = moment(race.current_period.end, "YYYY-MM-DD").locale("pt-br").format('DD/MM');
 
 		$.each(race.prices, function(j, price) {
 			race.prices[j].beginning = moment(price.beginning).format('DD/MM');
 			race.prices[j].end = moment(price.end).format('DD/MM');
 		});
-
+		
+		race.hasChampionship = (race.championships != null && race.championships.length > 0);
+		
 		var rendered = Mustache.render(template, race);
 		$('#event-races').append(rendered);
 	});
