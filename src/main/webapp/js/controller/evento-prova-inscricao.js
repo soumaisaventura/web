bd2c6cabeed69f49758675fa31e58c88159efee3
lusabo <lucianosantosborges@gmail.com>
2016-01-21 13:30:17 -0300

    Nova forma de montar a equipe.

--- src/main/webapp/js/controller/evento-prova-inscricao.js
+++ src/main/webapp/js/controller/evento-prova-inscricao.js
@@ -1,16 +1,74 @@
 $(function() {
+
+	/**
+	 * Configurações iniciais
+	 */
 	moment.locale("pt-br");
 	numeral.language('pt-br');
 	numeral.defaultFormat('0.00');
 
+	/**
+	 * Variáveis
+	 */
+	var user;
 	var memberIds = [];
 	var eventId = $("#event_id").val();
 	var raceId = $("#race_id").val();
 
-	$("#categoryId").focus();
+	/**
+	 * Carrega o id do facebook para poder compartilhar a inscrição
+	 */
+	LogonProxy.getOAuthAppIds().done(getOAuthAppIdsOk);
 
+	/**
+	 * Carrega os dados da corrida
+	 */
+	RaceProxy.loadSummary(raceId, eventId).done(loadSummaryOk);
+
+	/**
+	 * Carrega a combo de categorias
+	 */
+	RaceProxy.findCategories(raceId, eventId).done(loadCategoriesOk);
+
+	/**
+	 * Carrega o usuário que está logado
+	 */
+	if (!(user = App.getLoggedInUser())) {
+		App.handle401();
+	}
+
+	console.log('--------------- user --------------- ');
+	console.log(user);
+
+	/**
+	 * Coloca o usuário logado na lista de membros da equipe
+	 */
+	RaceProxy.getOrder(raceId, eventId, user.id).done(function(order) {
+		console.log('--------------- order --------------- ');
+		console.log(order);
+		console.log(memberIds);
+		getOrderOk(order, memberIds, false);
+	});
+
+	/**
+	 * Ajusta a tabela para quando for visualizada em celulares
+	 */
+	$('#members-list').footable({
+		breakpoints : {
+			phone : 450
+		}
+	});
+
+	/**
+	 * Coloca o foco na combo de categoria Trigger que verifica se o tamanho da
+	 * equipe é igual a 1
+	 */
+	$("#categoryId").focus();
 	$("#categoryId").on("change", function() {
 		var teamsize = $(this).find('option:selected').data("teamsize");
+
+		console.log('teamsize = ' + teamsize);
+
 		if (teamsize === 1) {
 			$("#pesquisa-atleta").hide();
 		} else {
@@ -19,26 +77,10 @@ $(function() {
 		updateTotal();
 	});
 
-	$('#members-list').footable({
-		breakpoints : {
-			phone : 450
-		}
-	});
-
-	LogonProxy.getOAuthAppIds().done(getOAuthAppIdsOk);
-	RaceProxy.loadSummary(raceId, eventId).done(loadSummaryOk);
-	RaceProxy.findCategories(raceId, eventId).done(loadCategoriesOk);
-
-	var user;
-
-	if (!(user = App.getLoggedInUser())) {
-		App.handle401();
-	}
-
-	RaceProxy.getOrder(raceId, eventId, user.id).done(function(order) {
-		getOrderOk(order, memberIds, false);
-	});
-
+	/**
+	 * Transforma o campo para inserir membro para o tipo autocomplete A função
+	 * _renderItem está sendo usada para mostrar a imagem do atleta na busca
+	 */
 	$("#members_ids").autocomplete({
 		source : function(request, response) {
 			UserProxy.search(request.term, memberIds).done(function(data) {
@@ -47,50 +89,44 @@ $(function() {
 		},
 		minLength : 3,
 		select : function(event, ui) {
-			$("#members-id").val(ui.item.value);
+			$("#memberId").val(ui.item.value);
 			$("#members_ids").val(ui.item.label);
+			addAthlete(raceId, eventId, memberIds);
 			return false;
 		},
 		focus : function(event, ui) {
-			$("#members-id").val(ui.item.value);
+			$("#memberId").val(ui.item.value);
 			$("#members_ids").val(ui.item.label);
+
 			return false;
 		}
-	});
+	}).autocomplete("instance")._renderItem = function(ul, item) {
+		return $("<li></li>").data("data-value", item).append("<img src='" + item.thumbnail + "'> " + item.label).appendTo(ul);
+	};
 
-	$("#bt-add-athlete").click(function() {
-
-		var members = $("#members_ids");
-		var membersId = $("#members-id");
-
-		$("#members_ids-message").hide();
-		if (membersId) {
-			RaceProxy.getOrder(raceId, eventId, membersId.val()).done(function(order) {
-				getOrderOk(order, memberIds, true);
-			});
-			membersId.val("");
-			members.val("");
-
-		} else {
-			$("#members_ids-message").html("Para incluir um atleta na equipe ele precisa se cadastrar no site e ativar a conta.").show();
-			members.focus();
-		}
-	});
-
+	/**
+	 * Remove um membro da equipe
+	 */
 	$("#members-list").on("click", ".remove", function(e) {
 		e.preventDefault();
-
 		var teamId = $(this).data("id");
+		console.log('Removendo atleta = ' + teamId);
 		memberIds.splice($.inArray(teamId, memberIds), 1);
-
 		var row = $(this).parents('tr:first');
 		$('#members-list').data('footable').removeRow(row);
 		updateTotal();
 	});
 
+	/**
+	 * Confirma a inscrição
+	 */
+
 	$("form").submit(function(event) {
 		event.preventDefault();
 		$("[id$='-message']").hide();
+
+		console.log('memberIds');
+		console.log(memberIds);
 
 		var data = {
 			'team_name' : $("#teamName").val(),
@@ -101,31 +137,23 @@ $(function() {
 	});
 });
 
-function getOrderOk(order, memberIds, deletable) {
-	if (order) {
-		memberIds.push(order[0].id);
-		addRowOnMemberList(order[0], deletable);
-		updateTotal();
-		$("#summary-section, #submit-button-section, #members-section").show();
-	} else {
-		console.log("Fora do período de inscrição!");
-		var url = $("#event_link").attr("href");
-		window.location.href = url;
-	}
-}
+/* ============================CALLBACK============================ */
 
 function getOAuthAppIdsOk(data) {
+	console.log('1: getOAuthAppIdsOk');
 	$("#facebook-appid").val(data.facebook);
 }
 
 function loadSummaryOk(race) {
-	$(".race-name").text(race.event.name)
+	console.log('2: loadSummaryOk');
+	$("#race-name").text(race.event.name)
 	$("#race-description").text(race.description)
 	$("#race-date").text(App.parsePeriod(race.period));
 	$("#race-city").text(App.parseCity(race.event.location.city));
 }
 
 function loadCategoriesOk(categories) {
+	console.log('3: loadCategoriesOk');
 	if (categories) {
 		$.each(categories, function(i, category) {
 			var option = new Option(category.name, category.id);
@@ -141,7 +169,76 @@ function loadCategoriesOk(categories) {
 	}
 }
 
+function getOrderOk(order, memberIds, deletable) {
+	console.log('4: getOrderOk');
+	console.log('--- antes ---');
+	console.log(memberIds);
+	if (order) {
+		memberIds.push(order[0].id);
+		addRowOnMemberList(order[0], deletable);
+		updateTotal();
+		$("#summary-section, #submit-button-section, #members-section").show();
+	} else {
+		console.log("Fora do período de inscrição!");
+		var url = $("#event_link").attr("href");
+		window.location.href = url;
+	}
+	console.log('--- depois--');
+	console.log(memberIds);
+}
+
+function addRowOnMemberList(athlete, deletable) {
+	console.log('5: addRowOnMemberList');
+	athlete.deletable = deletable;
+	athlete.formmated_race_price = numeral(athlete.race_price).format();
+
+	var template = $("#member-template");
+	var rendered = Mustache.render(template.html(), athlete);
+	$('#members-list').data('footable').appendRow(rendered);
+}
+
+function updateTotal() {
+	console.log('6: updateTotal');
+	var total = 0;
+	$(".ammount").each(function() {
+		total += $(this).data("ammount");
+	});
+	$("#total").text(numeral(total).format());
+}
+
+function convertToLabelValueStructureFromUser(data) {
+	console.log('7: convertToLabelValueStructureFromUser');
+	var newData = [];
+	$.each(data, function() {
+		newData.push({
+			"label" : this.name,
+			"value" : this.id,
+			"thumbnail" : this.picture.thumbnail
+		});
+	});
+	return newData;
+}
+
+function addAthlete(raceId, eventId, memberIds) {
+	console.log('8: addAthlete');
+	var memberId = $("#memberId");
+	var members_ids = $("#members_ids");
+	$("#members_ids-message").hide();
+
+	if (memberId) {
+		RaceProxy.getOrder(raceId, eventId, memberId.val()).done(function(order) {
+			getOrderOk(order, memberIds, true);
+		});
+		memberId.val(""); // Limpa o campo
+		members_ids.val(""); // Limpa o campo
+	} else {
+		$("#members_ids-message").html("Para incluir um atleta na equipe ele precisa se cadastrar no site e ativar a conta.").show();
+		members.focus();
+	}
+}
+
 function registrationOk(data) {
+	console.log('9: registrationOk');
 	$("[id$='-message']").hide();
 	var url = App.getContextPath() + "/inscricao/" + data;
 
@@ -165,18 +262,8 @@ function registrationOk(data) {
 	});
 }
 
-function registrationFailed(request) {
-	switch (request.status) {
-		case 500:
-			$("#members-message")
-					.html(
-							"Ocorreu um erro interno no servidor e sua inscrição não foi concluída. Informe ao administrador pelo e-mail: contato@soumaisaventura.com.br")
-					.show();
-			break;
-	}
-}
-
 function shareOnFacebook() {
+	console.log('10: shareOnFacebook');
 	var raceUrl = App.getBaseUrl() + "/prova/" + $("#id").val();
 	var url = "";
 	url += "http://www.facebook.com/dialog/feed";
@@ -190,34 +277,4 @@ function shareOnFacebook() {
 	url += "&actions=[{ name: 'Quero me inscrever agora mesmo!', link: '" + raceUrl + "/inscricao' }]";
 
 	window.open(url, '_blank');
-}
-
-function convertToLabelValueStructureFromUser(data) {
-	var newData = [];
-	$.each(data, function() {
-		newData.push({
-			"label" : this.name,
-			"value" : this.id
-		});
-	});
-	return newData;
-}
-
-function addRowOnMemberList(athlete, deletable) {
-	athlete.deletable = deletable;
-	athlete.formmated_race_price = numeral(athlete.race_price).format();
-
-	var template = $("#member-template");
-	var rendered = Mustache.render(template.html(), athlete);
-	$('#members-list').data('footable').appendRow(rendered);
-}
-
-function updateTotal() {
-	var total = 0;
-
-	$(".ammount").each(function() {
-		total += $(this).data("ammount");
-	});
-
-	$("#total").text(numeral(total).format());
 }
